e574eebcfe1b33028500f126e3e2045e77fce761
Rhhawk <henrique.scosta@live.com>
2024-05-27 14:23:22 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,29 @@
+/* 
+
+Variaveis
+um espaço de memoria reservado para guardar oque vc quiser dentro
+
+- var - cod antigo - nao use
+- let - voce pode alterar o valor depois 
+- const -> constante segurança para nao mudar o codigo
+
+*/
+
+/* FUNÇÃO = UM TREXO DE CODIGO SO EXECUTADO SO QUANDO FOR CHAMADO 
+
+*/
+
+const form = document.querySelector(".formulario")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao() {
+   form.style.left = "50%"
+   form.style.transform = "translateX(-50%)"
+   mascara.style.visibility = "visible"
+}
+
+function cliqueiNaMascara() {
+mascara.style.visibility = "hidden"
+form.style.left = "-300px"
+
+}
